2a942e708c390a3d9f0b1f6defb68bed97e625ae
Даниил <Daniil.Zanin@mail.ru>
2024-03-05 22:49:12 +0300

    first commit

---
+++ Algebra_Fractal/Algebra_Fractal/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Algebra_Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1100, 1100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(1228, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Iterations ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(1155, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "R";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LawnGreen;
+            this.label3.Location = new System.Drawing.Point(1154, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "G";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.label4.Location = new System.Drawing.Point(1155, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "B";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1154, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1154, 338);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1154, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(1153, 55);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(245, 27);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(1194, 93);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(202, 27);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(1194, 135);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(204, 27);
+            this.numericUpDown3.TabIndex = 10;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(1195, 179);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(203, 27);
+            this.numericUpDown4.TabIndex = 11;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button1.Location = new System.Drawing.Point(1153, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 65);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button2.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button2.Location = new System.Drawing.Point(1153, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 65);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button3.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button3.Location = new System.Drawing.Point(1153, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(245, 68);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "To start";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1430, 980);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Algebra_Fractal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Algebra_Fractal/Algebra_Fractal/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algebra_Fractal
+{
+    public partial class Form1 : Form
+    {
+        Bitmap B = new Bitmap(1100, 1100);
+        Graphics g;
+        int CNT = 100, stack_size = 0;
+        int D1 = 10, D2 = 15, D3 = 20;
+        double[] stack = new double[200];
+        double minX = -2.5, minY = -2, maxX = 1.5, maxY = 2;
+        double MaxLen = 10;
+        bool Clicked = false;
+        Point MousePos;
+        Color[] pallete = new Color[1000];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private int ToInt(double x)
+        {
+            return int.Parse(Math.Round(x).ToString());
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Clicked = true;
+            MousePos = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Clicked = false;
+            pictureBox1.Image = B;
+            Add(minX);
+            Add(maxX);
+            Add(minY);
+            Add(maxY);
+            Complex a = ToPoint(MousePos);
+            Complex b = ToPoint(new Point(e.X, e.Y));
+            minX = Math.Min(a.R, b.R);
+            maxX = Math.Max(a.R, b.R);
+            minY = Math.Min(a.I, b.I);
+            maxY = Math.Max(a.I, b.I);
+            label5.Text = "Center:\n(" + ((minX + maxX) / 2).ToString() + ";" + ((minY + maxY) / 2).ToString() + ")";
+            label6.Text = "Width: " + (maxX - minX).ToString();
+            label7.Text = "Height: " + (maxY - minY).ToString();
+            Draw();
+        }
+
+
+        void Add(double elem)
+        {
+            stack[stack_size] = elem;
+            ++stack_size;
+        }
+
+        double Pop()
+        {
+            double res = stack[stack_size - 1];
+            --stack_size;
+            return res;
+        }
+        private int NextColor(int C, ref int D)
+        {
+            if (C + D >= 255 || C + D < 0)
+                D = -D;
+            C += D;
+            return C;
+        }
+        private void Draw()
+        {
+            for (int x = 0; x < B.Width; ++x)
+            {
+                for (int y = 0; y < B.Height; ++y)
+                {
+                    Complex C = ToPoint(new Point(x, y));
+                    Complex Z = new Complex(0, 0);
+                    int C1 = 0, C2 = 0, C3 = 0;
+                    for (int i = 0; i < CNT; ++i)
+                    {
+                        if (Z.SquareLength() > MaxLen)
+                            break;
+                        C1 = NextColor(C1, ref D1);
+                        C2 = NextColor(C2, ref D2);
+                        C3 = NextColor(C3, ref D3);
+                        Z = Z * Z + C;
+                    }
+                    B.SetPixel(x, y, Color.FromArgb(C1, C2, C3));
+                }
+            }
+            pictureBox1.Image = B;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (stack_size != 0)
+            {
+                maxY = Pop();
+                minY = Pop();
+                maxX = Pop();
+                minX = Pop();
+                Draw();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stack_size = 0;
+            minX = -2.5;
+            maxX = 1.5;
+            minY = -2;
+            maxY = 2;
+            Draw();
+        }
+
+        private void Create_Pallete()
+        {
+            pallete[0] = Color.FromArgb(0, 0, 0);
+            for (int i = 1; i < CNT; ++i)
+            {
+                pallete[i] = Color.FromArgb(NextColor(pallete[i - 1].R, ref D1), NextColor(pallete[i - 1].G, ref D2), NextColor(pallete[i - 1].B, ref D3));
+            }
+        }
+        private void Draw1()
+        {
+            Create_Pallete();
+            for (int x = 0; x < B.Width; ++x)
+            {
+                for (int y = 0; y < B.Height; ++y)
+                {
+                    Complex C = ToPoint(new Point(x, y));
+                    Complex Z = new Complex(0, 0);
+                    int i;
+                    for (i = 0; i < CNT; ++i)
+                    {
+                        if (Z.SquareLength() > MaxLen)
+                            break;
+                        Z = Z * Z + C;
+                    }
+                    if (i < CNT)
+                    {
+                        double log_zn = Math.Log(Z.SquareLength(), 2) / 2;
+                        double nu = Math.Log(log_zn / Math.Log(2, 2), 2) / Math.Log(2, 2);
+                        double ind = i + 1 - nu;
+                        Color color1 = pallete[ToInt(Math.Floor(ind)) % 1000];
+                        Color color2 = pallete[(ToInt(Math.Floor(ind)) + 1) % 1000];
+                        Color color;
+                        Random r = new Random();
+                        int aR = r.Next() % CNT;
+                        int bR = r.Next() % CNT;
+                        int aG = r.Next() % CNT;
+                        int bG = r.Next() % CNT;
+                        int aB = r.Next() % CNT;
+                        int bB = r.Next() % CNT;
+                        color = Color.FromArgb(ToInt(ToDouble(aR * color1.R + bR * color2.R) / (aR + bR)),
+                            ToInt(ToDouble(aG * color1.G + bG * color2.G) / (aG + bG)),
+                            ToInt(ToDouble(aB * color1.B + bB * color2.B) / (aB + bB)));
+                        B.SetPixel(x, y, color);
+                    }
+                    else
+                        B.SetPixel(x, y, Color.Black);
+                }
+            }
+            pictureBox1.Image = B;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            CNT = int.Parse(numericUpDown1.Value.ToString());
+            CNT *= 2;
+            Draw();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private double ToDouble(int x)
+        {
+            return double.Parse(x.ToString());
+        }
+
+        private Point ToPixel(Complex a)
+        {
+            Point Res = new Point(ToInt((a.R - minX) / (maxX - minX) * B.Width), ToInt((a.I - minY) / (maxY - minY) * B.Height));
+            Res.Y = B.Height - Res.Y;
+            return Res;
+        }
+        private Complex ToPoint(Point d)
+        {
+            d.Y = B.Height - d.Y;
+            Complex Res = new Complex(d.X * (maxX - minX) / B.Width + minX, d.Y * (maxY - minY) / B.Height + minY);
+            return Res;
+        }
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            D1 = int.Parse(numericUpDown2.Value.ToString());
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            D2 = int.Parse(numericUpDown3.Value.ToString());
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            D3 = int.Parse(numericUpDown4.Value.ToString());
+        }
+
+
+    }
+}
+
+public class Complex
+{
+    public double R, I;
+    public Complex(double a = 0, double b = 0)
+    {
+        this.R = a;
+        this.I = b;
+    }
+
+    public Complex Conjugate()
+    {
+        return new Complex(R, -I);
+    }
+
+    public double SquareLength()
+    {
+        return R * R + I * I;
+    }
+
+    public double Abs()
+    {
+        return Math.Sqrt(R * R + I * I);
+    }
+    public static Complex operator +(Complex x, Complex y)
+    {
+        return new Complex(x.R + y.R, x.I + y.I);
+    }
+
+    public static Complex operator -(Complex x, Complex y)
+    {
+        return new Complex(x.R - y.R, x.I - y.I);
+    }
+
+    public static Complex operator *(Complex x, Complex y)
+    {
+        return new Complex(x.R * y.R - x.I * y.I, x.R * y.I + x.I * y.R);
+    }
+
+    public static Complex operator /(Complex x, double k)
+    {
+        return new Complex(x.R / k, x.I / k);
+    }
+
+    public static Complex operator *(Complex x, double k)
+    {
+        return new Complex(x.R * k, x.I * k);
+    }
+
+    public static Complex operator *(double k, Complex x)
+    {
+        return new Complex(x.R * k, x.I * k);
+    }
+
+    public static Complex operator /(Complex x, Complex y)
+    {
+        return (x * y.Conjugate()) / y.SquareLength();
+    }
+}
